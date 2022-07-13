f541c17f6c10a85b27be4a20bad5cba085114229
ubukawa <ubukawa-t96dq@mlit.go.jp>
2022-07-12 23:09:42 -0400

    rev

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const fs = require('fs')
 const Queue = require('better-queue')
 const { spawn } = require('child_process')
 const Parser = require('json-text-sequence').parser
-
+const tilebelt = require('@mapbox/tilebelt')
 
 const srcdb = config.get('srcdb')
 const ogr2ogrPath = config.get('ogr2ogrPath')
@@ -36,12 +36,15 @@ const sleep = (wait) => {
 const queue = new Queue(async (t, cb) => {
     const startTime = new Date()
     const key = t.key
+    const tile = t.tile
+    const [z, x, y] = tile
     const gjsPath = `${geojsonsDir}/inter-${key}.geojsons`
     const tmpPath = `${mbtilesDir}/part-${key}.mbtiles` //`${mbtilesDir}/part-${z}-${x}-${y}.mbtiles`
     const dstPath = `${mbtilesDir}/${key}.mbtiles` //`${mbtilesDir}/${z}-${x}-${y}.mbtiles`
+    const bbox = tilebelt.tileToBBOX([x, y, z])
 
     keyInProgress.push(key)
-    console.log(`${keyInProgress} on process`)
+    console.log(`${keyInProgress} in progress`)
 
     const FSstream = fs.createWriteStream(gjsPath, fsOptions)
 
@@ -93,9 +96,9 @@ const queue = new Queue(async (t, cb) => {
         '-f', 'GeoJSONSeq',
         '-lco', 'RS=YES',
         '/vsistdout/',
-        //'-clipdst', 0, 52.4827, 5.625, 55.76573,
-        //srcdb.url
-        `small-data/${key}.geojson`
+        '-clipdst', bbox[0], bbox[1], bbox[2], bbox[3],
+        srcdb.url
+        //`small-data/${key}.geojson`
     ])
     
     //just in case (from here)
@@ -120,9 +123,12 @@ const queue = new Queue(async (t, cb) => {
 
 
 const queueTasks = () => {
-    for (let key of ['bndl1', 'bndl2', 'bndl3', 'bndl4', 'bndl5', 'bndl6']){
+    for (let tile of [[6,32,20],[6,32,21],[6,32,22],[6,32,23],[6,33,20],[6,33,21],[6,33,22]]){
+    //for (let key of ['bndl1', 'bndl2', 'bndl3', 'bndl4', 'bndl5', 'bndl6']){
+        const key = `${tile[0]}-${tile[1]}-${tile[2]}`
         queue.push({
-            key: key
+            key: key,
+            tile: tile
         })
     }
 }
